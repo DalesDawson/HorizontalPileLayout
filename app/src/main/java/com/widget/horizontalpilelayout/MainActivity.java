25e6fef80327921640583e0b7b4355002d085e50
DalesDawson <422564193@qq.com>
2020-08-21 11:59:50 +0800

    Initial commit

---
+++ app/src/main/java/com/widget/horizontalpilelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.widget.horizontalpilelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
